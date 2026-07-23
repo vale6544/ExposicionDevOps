716b5c2386bf3697bfd0b04828173a17a0630d4f
vale6544 <ibararvale@gmail.com>
2026-07-22 23:39:31 -0500

    Mi primer pipeline

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // función principal
+        Func<int, int, int> sumar = (a, b) => a + b;
+
+        // prueba automática
+        int resultado = sumar(2, 3);
+
+        if (resultado != 5)
+        {
+            // si falla, excepción para detener automatización
+            throw new Exception("La prueba falló porque esperábamos un 5, pero obtuvimos un " + resultado);
+        }
+
+        Console.WriteLine("Prueba exitosa. El código está validado.");
+    }
+}
